9e7635f02bce2d55fcb804b0c22aa67f5a595672
Harshith Vyas <harshithvyas@gmail.com>
2025-09-29 12:13:38 +0530

    session-06

--- catalogue.sh
+++ catalogue.sh
@@ -16,7 +16,7 @@ echo "Script started executed at: $(date)" &>>$LOG_FILE
 
 if [ $USERID -ne 0 ]; then
     echo -e "$R ERROR:: Please run this script with root privilege $N"
-    exit 1 # failure is other than 0
+    exit 1
 fi
 
 VALIDATE() {
@@ -32,34 +32,54 @@ VALIDATE() {
 dnf module disable nodejs -y &>>$LOG_FILE
 VALIDATE $? "Disabling NodeJS"
 dnf module enable nodejs:20 -y &>>$LOG_FILE
-VALIDATE $? " Enabling NodeJS 20"
+VALIDATE $? "Enabling NodeJS 20"
 dnf install nodejs -y &>>$LOG_FILE 
-VALIDATE $? " Installing NodeJS 20"
+VALIDATE $? "Installing NodeJS 20"
 
+#### System user and app directory ####
 useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
 VALIDATE $? "Creating system user"
 
-mkdir /app
+mkdir -p /app
 VALIDATE $? "Creating app directory"
+
+#### Download and setup catalogue ####
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOG_FILE
 VALIDATE $? "Downloading catalogue application"
 cd /app
 VALIDATE $? "Changing to app directory"
 unzip /tmp/catalogue.zip &>>$LOG_FILE
-VALIDATE $? "unzip catalogue"
+VALIDATE $? "Unzip catalogue"
 npm install &>>$LOG_FILE
 VALIDATE $? "Install dependencies"
 cp catalogue.service /etc/systemd/system/catalogue.service
 VALIDATE $? "Copy systemctl service"
 systemctl daemon-reload
 systemctl enable catalogue &>>$LOG_FILE
-VALIDATE $? "Enable catalogue"
+VALIDATE $? "Enable catalogue service"
 
-cp mongo.repo /etc/yum.repos.d/mongo.repo
-VALIDATE $? "Copy mongo repo"
-dnf install mongodb-mongosh -y &>>$LOG_FILE
-VALIDATE $? "Install MongoDB client"
-mongosh --host $MONGODB-HOST </app/db/master-data.js &>>$LOG_FILE
+#### MongoDB setup ####
+tee /etc/yum.repos.d/mongodb-org-7.0.repo <<EOF &>>$LOG_FILE
+[mongodb-org-7.0]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/redhat/9/mongodb-org/7.0/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-7.0.asc
+EOF
+VALIDATE $? "Add MongoDB 7 repo"
+
+dnf install -y mongodb-org &>>$LOG_FILE
+VALIDATE $? "Install MongoDB 7"
+
+systemctl enable mongod &>>$LOG_FILE
+systemctl start mongod &>>$LOG_FILE
+VALIDATE $? "Start MongoDB service"
+
+#### Load catalogue data ####
+mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE
 VALIDATE $? "Load catalogue products"
-systemctl restart catalogue
-VALIDATE $? "Restarted catalogue"
+
+#### Start catalogue service ####
+systemctl restart catalogue &>>$LOG_FILE
+VALIDATE $? "Restarted catalogue service"
